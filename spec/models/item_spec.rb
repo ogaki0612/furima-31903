b4c69c079a986e6228e177968ae57eeeeeba319c
ogaki0612 <kog.v.0612@gmail.com>
2020-12-10 11:17:17 +0900

    レビュー後修正　12/10 2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -58,12 +58,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'priceの値が300より小さいと出品できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'priceの値が999999999より大きいと出品できない' do
-        @item.price = '1000000000'
+        @item.price = 1_000_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 999999999')
       end

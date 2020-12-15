7213c0e709baf4969c6fbf9ddd9e7a1bbc8f5b40
ogaki0612 <kog.v.0612@gmail.com>
2020-12-15 17:48:39 +0900

    テストコード実装

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_address do
+    token {"tok_abcdefghijk00000000000000000"}
+    postal_code {"123-4567"}
+    prefecture_id {1}
+    address_1 {"テスト市"}
+    address_2 {"テスト町1-23"}
+    building {"テストマンション101"}
+    phone_number { "09012345678" }
+
+    
+  end
+end
